1a1e63ba19786db59a15c0facaa903db6f11d866
Victor <victorhaeggman@gmail.com>
2017-11-18 01:55:01 +0100

    Very basic start to level manager

---
+++ Assets/Scripts/BackEnd/LevelManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public static LevelManager instance = null;
+    private int level = 0;
+    private int[] levelorder;
+    private GameObject[] players;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        InitGame();
+    }
+
+
+    void InitGame()
+    {
+
+
+    }
+
+    public void startLevel()
+    {
+
+        //loadlevelstuff
+        players = new GameObject[players.Length + GameObject.FindGameObjectsWithTag("Player").Length];
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/BackEnd/LevelOrderGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOrderGenerator : MonoBehaviour {
+    private ArrayList AlchemyLevels;
+	// Use this for initialization
+	void Start () {
+        AlchemyLevels = new ArrayList();
+         
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
