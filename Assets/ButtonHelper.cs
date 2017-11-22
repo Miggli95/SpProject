5d9dfefffcc8f7eeb285b6b0eee24fc87be96c07
sync93 <timpaboi@live.se>
2017-11-22 17:30:51 +0100

    started on button reminder, shadow fixes

---
+++ Assets/ButtonHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHelper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag ("Player"))
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
