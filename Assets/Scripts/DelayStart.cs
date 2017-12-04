51f5f0e6b20b17f783c6d3ec761ccf1f018bd776
sync93 <timpaboi@live.se>
2017-12-04 20:17:49 +0100

    added 3..2..1.. go timer, will work on finilizing text obj to teach player

---
+++ Assets/Scripts/DelayStart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayStart : MonoBehaviour {
+
+    public GameObject countDown;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("DelayStart1");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator DelayStart1()
+    {
+        Time.timeScale = 0;
+        float pauseTime = Time.realtimeSinceStartup + 3f;
+        while (Time.realtimeSinceStartup < pauseTime)
+            yield return 0;
+        countDown.gameObject.SetActive(false);
+        Time.timeScale = 1;
+       // print("working");
+        
+         
+        }
+
+
+    }
+
+
