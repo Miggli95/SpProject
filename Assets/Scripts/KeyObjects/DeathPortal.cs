2bed873b2f39ad3966f67ca13e16f6a5e83ee735
Victor <victorhaeggman@gmail.com>
2017-12-05 16:41:10 +0100

    Made Hot Potato playable, added grimorie to scene.

--- Assets/Scripts/KeyObjects/DeathPortal.cs
+++ Assets/Scripts/KeyObjects/DeathPortal.cs
@@ -5,17 +5,36 @@ using UnityEngine;
 public class DeathPortal : MonoBehaviour {
 
     private List<Controller2D> players;
+    private Timer timmy;
     private float LifeTime;
     private float TimeAlive;
-
-	
-	// Update is called once per frame
-	void Update ()
+    private int score;
+    private void Start()
+    {
+        timmy = GameObject.Find("UI Camera").GetComponent<Timer>();
+    }
+    // Update is called once per frame
+    void Update ()
     {
         if (TimeAlive >= LifeTime)
         {
+            switch(players.Count){
+                case 1:
+                    score = 40;
+                    break;
+                case 2:
+                    score = 20;
+                    break;
+                case 3:
+                    score = 10;
+                    break;
+                case 4:
+                    score = 10;
+                    break;
+            }
             foreach (Controller2D p in players)
             {
+                timmy.runeGet(p.name, score);
                 p.doDeath();
             }
             Destroy(this.gameObject);
