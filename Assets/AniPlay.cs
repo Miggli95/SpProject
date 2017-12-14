0681a61ad582f636fbde3a39bbf608360782de0e
sync93 <timpaboi@live.se>
2017-12-14 20:24:09 +0100

    fixed some collders and tried some stuff in controller ani

--- Assets/AniPlay.cs
+++ Assets/AniPlay.cs
@@ -4,33 +4,44 @@ using System.Collections;
 public class AniPlay : MonoBehaviour
 {
 
-    public bool enterTrig;
+    
    
-    private Animator anim;
+    public Animator anim;
   
 
     // Use this for initialization
     void Start()
     {
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        anim = GetComponent<Animator>();
+        
     }
-
-
-    
-    void OnTriggerEnter(Collider other)
+    void OnTriggerExit(Collider other)
     {
+        print("work");
         if (other.CompareTag("Player"))
         {
 
-            anim.SetBool("SimpleBearAni.anim", true);
-            GetComponent<AudioSource>().Play();
-            print("works");
+            anim.Play("SimpleBearAni");
 
         }
+
+
+        void OnTriggerEnter(Collider other)
+    {
+        print("work");
+        if (other.CompareTag("Player"))
+        {
+
+            anim.Play("SimpleBearAni");
+
+
+        }
+
+
     }
 }
