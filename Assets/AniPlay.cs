24bc322d2b0e5220e7b3df389ccfddf48a50a24d
sync93 <timpaboi@live.se>
2017-12-14 19:53:43 +0100

    changes in lvl + ani script

---
+++ Assets/AniPlay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniPlay : MonoBehaviour {
+
+   // public GameObject BearEvenHungrier2;
+    public Animator _animator = null;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+         if(other.gameObject.tag == "Player")
+        {
+
+            _animator.Play("SimpleBearAni");
+            
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        _animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
