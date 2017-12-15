a4990a972de916a5027b818efaef5aeb5b947c17
sync93 <timpaboi@live.se>
2017-12-15 18:01:25 +0100

    spider bomb ani + speedrun cleanup prisons

--- Assets/PrisonB.cs
+++ Assets/PrisonB.cs
@@ -2,35 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PrisonScript : MonoBehaviour {
+public class PrisonB : MonoBehaviour
+{
     private BoxCollider boxy;
     private List<Controller2D> players;
     private float countdown = 0f;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         boxy = GetComponent<BoxCollider>();
         players = new List<Controller2D>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		if(players.Count == 0)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (players.Count == 0)
         {
             countdown = 0f;
         }
-        if(countdown > 0f)
+        if (countdown > 0f)
         {
             countdown -= Time.deltaTime;
-            if(countdown < 0f)
+            if (countdown < 0f)
             {
                 var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 1.5f, this.transform.position.z);
-                cube.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y-3, this.transform.localScale.z);
+                cube.transform.position = new Vector3(this.transform.position.x , this.transform.position.y - 2f, this.transform.position.z);
+                cube.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y - 2, this.transform.localScale.z);
                 countdown = 0f;
             }
         }
-        
-	}
+
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetType() == typeof(CharacterController) && !other.GetComponent<Controller2D>().isGhost)
@@ -52,15 +55,15 @@ public class PrisonScript : MonoBehaviour {
                         countdown = 5f;
                         break;
                     default:
-                        countdown = 3f;
+                        countdown = 4f;
                         break;
-                   
+
 
                 }
             }
             var player = other.GetComponent<Controller2D>();
             players.Add(player);
-            
+
         }
     }
     private void OnTriggerExit(Collider other)
@@ -72,7 +75,7 @@ public class PrisonScript : MonoBehaviour {
             {
                 players.Remove(player);
             }
-           
+
 
         }
     }
