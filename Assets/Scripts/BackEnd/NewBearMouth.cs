cf2a688535ad55b83fb8f87ca103ef7468e02612
Victor <victorhaeggman@gmail.com>
2017-12-15 16:29:58 +0100

    Added prison escape script

---
+++ Assets/Scripts/BackEnd/NewBearMouth.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBearMouth : MonoBehaviour
+{
+    public Timer score;
+
+    // Use this for initialization
+    void Start()
+    {
+        score = GameObject.Find("UI Camera").GetComponent<Timer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.GetType() == typeof(CharacterController) && !other.GetComponent<Controller2D>().isGhost)
+        {
+            // CameraScript camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraScript>();
+            // camera.RemovePlayer(other.gameObject.name);
+
+            if (other.GetComponent<Controller2D>().getAlive())
+            {
+                switch (this.name)
+                {
+                    case "30point":
+                        score.runeGet(other.name, 30);
+                        despawnFirst();
+                        other.GetComponent<Controller2D>().doDeath();
+                        this.name = "20point";
+                        break;
+                    case "20point":
+                        score.runeGet(other.name, 20);
+                        despawnSecond();
+                        other.GetComponent<Controller2D>().doDeath();
+                        this.name = "10point";
+                        break;
+                    case "10point":
+                        score.runeGet(other.name, 10);
+                        despawnThird();
+                        other.GetComponent<Controller2D>().doDeath();
+                        break;
+
+                }
+            }
+
+        }
+    }
+    private void despawnFirst()
+    {
+        //add more animation/interesting stuff
+        GameObject.Find("BearEvenHungrier").SetActive(false);
+        GameObject.Find("BearHungry").SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+    private void despawnSecond()
+    {
+        //add more animation/interesting stuff
+
+        GameObject.Find("BearHungry").SetActive(false);
+        GameObject.Find("Bear").SetActive(true);
+        this.gameObject.SetActive(false);
+
+    }
+    private void despawnThird()
+    {
+        //add more animation/interesting stuff
+        GameObject.Find("Bear").SetActive(false);
+        this.gameObject.SetActive(false);
+
+    }
+}
---
+++ Assets/Scripts/BackEnd/PrisonScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrisonScript : MonoBehaviour {
+    private BoxCollider boxy;
+    private List<Controller2D> players;
+    private float countdown = 0f;
+    // Use this for initialization
+    void Start () {
+        boxy = GetComponent<BoxCollider>();
+        players = new List<Controller2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(players.Count == 0)
+        {
+            countdown = 0f;
+        }
+        if(countdown > 0f)
+        {
+            countdown -= Time.deltaTime;
+            if(countdown < 0f)
+            {
+                var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 1.5f, this.transform.position.z);
+                cube.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y-3, this.transform.localScale.z);
+                countdown = 0f;
+            }
+        }
+        
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetType() == typeof(CharacterController) && !other.GetComponent<Controller2D>().isGhost)
+        {
+            if (players.Count == 0)
+            {
+                countdown = 2f;
+            }
+            var player = other.GetComponent<Controller2D>();
+            players.Add(player);
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetType() == typeof(CharacterController) && !other.GetComponent<Controller2D>().isGhost)
+        {
+            var player = other.GetComponent<Controller2D>();
+            if (players.Contains(player))
+            {
+                players.Remove(player);
+            }
+           
+
+        }
+    }
+}
